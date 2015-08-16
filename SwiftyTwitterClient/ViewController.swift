568ebbd77a9c4b3db34965592c816a18ffc884e1
takahashi <ikmy0709@gmail.com>
2015-08-16 20:26:40 +0900

    Initial Commit

---
+++ SwiftyTwitterClient/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftyTwitterClient
+//
+//  Created by yuichiro_t on 2015/08/16.
+//  Copyright (c) 2015年 yuichiro_t. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
